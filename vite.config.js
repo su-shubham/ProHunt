1e27dc6e43c17b3fb274376ac594ae567478650e
su-shubham <shubhamprajapati8268@gmail.com>
2022-11-06 21:55:58 +0530

    ⤴️ Undefined server port

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   preview: {
     port: 3000
   },
