ebb5306467eaefc2de050c988d65854f153989a2
su-shubham <shubhamprajapati8268@gmail.com>
2022-11-06 18:57:53 +0530

    ❌Deleted unused files

--- src/components/Private.jsx
+++
@@ -1,5 +0,0 @@
-export default function Private(){
-    return (
-        <h1>Hello</h1>
-    )
-}
--- src/components/Public.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Public(){
-    return (
-        <h1>Public</h1>
-    )
-}
