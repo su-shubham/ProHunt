6167e333b81f396e51977b8c5f7b047f48d47cb1
Sarika11-06 <sarikaprajapati8268@gmail.com>
2022-12-04 15:42:40 +0530

    Changes Fixed👍

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,59 @@
 // import MainBody from './components/MainBody';
 // import Asidebar from './components/Asidebar';
 // import Todaylist from './components/Todaylist';
-import Foot from './components/Foot';
-import LandPage from './components/LandPage';
-import React from "react"
-import Fief from './components/Fief'
+import Foot from "./components/Foot";
+import LandPage from "./components/LandPage";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Fief from "./components/Fief";
+import AnimatedCursor from "react-animated-cursor";
+import { Expo } from "gsap";
 
 function App() {
+
+  const [isRendered, setIsRendered] = useState(false);
+  const toggle = () => {
+    if (window.scrollY > 700) {
+      setIsRendered(true);
+    } else {
+      setIsRendered(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggle);
+    return () => {
+      window.removeEventListener("scroll", toggle);
+    };
+  });
+
   return (
-    <div className="overflow-hidden no-scrollbar  ">
-    <header className="text-gray-600 body-font ">
-      <Fief/>
-    </header>
- <body>
-    {/* <div className="pt-20">
-        <div className="container mx-auto px-5">
-          <div className="pt-6 grid grid-cols-1 lg:grid-cols-6 lg:gap-6">
-            <div className="col-span-4 sm:col-span-4 lg:col-span-6">
-              <LandPage />
-            </div>
-            <div className='col-span-4 sm:col-span-4 lg:col-span-4'>
-              <Todaylist />
-            </div>
-            <div className="col-span-2 lg:col-span-2">
-              <Asidebar />
-            </div>
-          </div>
-        </div>
-      </div> */}
-    </body>
-    <footer>
-      <Foot/>
-    </footer> 
+    <div className="App">
+      <Fief />
+      <motion.a
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: isRendered ? 1 : 0,
+        }}
+        transition={{
+          ease: Expo.easeOut,
+          duration: 1,
+        }}
+        href="#top"
+        className="back-to-top"
+        title="Back to Top"
+      >
+        <p className="back-to-top__image">↑</p>
+      </motion.a>
+      <AnimatedCursor
+        innerSize={15}
+        outerSize={0}
+        color="0, 0, 0"
+        outerAlpha={0.2}
+        innerScale={0.5}
+      />
     </div>
-  ); 
+  );
 }
 
-export default App;  
+export default App;
---
+++ src/components/components/skeletons/SkeletonAsideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SkeletonElement from './SkeletonElement'
+
+function SkeletonAsideBar() {
+  return (
+        <div className="skeleton-wrapper light">
+    <div className='skeleton-asidebar'>
+        <div>
+            <SkeletonElement type="title"/>
+            <SkeletonElement type="text"/>
+      </div>
+      <div className="skeleton-votes">
+        <SkeletonElement type="box" />
+      </div>
+        </div>
+    </div>
+  )
+}
+
+export default SkeletonAsideBar
