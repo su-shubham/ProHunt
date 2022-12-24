eb666d9653e07153f8581c502a1a526eaaa60a96
Sarika11-06 <sarikaprajapati8268@gmail.com>
2022-12-24 10:53:46 +0530

    Changes Done🎇

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,32 +1,26 @@
-import React from 'react';
-
-import Advertisment from '../assets/advertisment.jpeg';
+import React from "react";
 
 export const Newsletter = () => {
   return (
-    <div className="mb-6">
-      <div className="mt-4 text-base font-bold mb-3 text-gray-900">
-        Newsletter
-      </div>
-      <div className="bg-white rounded shadow ">
+    <div className="mb-6 flex items-center justify-center min-h-screen">
+      <div className="">
         <div>
           <div className="p-5">
-            <span className="font-bold text-base text-gray-900 pb-2 flex">
-              Get the best new products in your inbox, every day 👇
-            </span>
-            <div>
-              <form action="submit">
-                <input
-                  type="text"
-                  id="hero-field"
-                  name="hero-field"
-                  className="text-f13 pl-4 w-full rounded  focus:bg-transparent border border-gray-300 focus:border-grey-500 outline-none placeholder-gray-500 py-1 px- leading-8 transition-colors duration-200 ease-in-out"
-                  placeholder="Your email"
-                />
-                <div className="mt-3">
-                  <button className="primary text-f11 font-bold bg-[#f43f5e] uppercase inline-flex text-white items-center bg-orange border-0 py-2 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 w-full">
-                    <span className="f13 text-center mx-auto">Subscribe</span>
-                  </button>
+            <div className="login-form bg-white w-[400px] mx-[65px] my-auto flex flex-col rounded-md shadow-2xl">
+              <form className="">
+                <h1 className="pt-9 py-9 pb-0 font-semibold text-3xl flex items-center justify-center mb-5">NewsLetter </h1>
+                <div className="content p-9 text-center outline-none relative">
+                    <label className="top-0 absolute  left-10 px-3 py-0 text-2xl text-black">Email</label>
+                  <div className="input-field px-3 py-1 ">
+                    <input
+                      type="email"
+                      autocomplete="nope"
+                      className="text-base block w-full px-3 py-[1px] border-0 border-b border-[#747474]  focus:ring-0"
+                    />
+                  </div>
+                </div>
+                <div className="action w-full flex justify-center items-center mb-8">
+                  <button className=" border-0 px-5 py-4 uppercase bg-[#f43f5e] text-[#fff] font-extrabold tracking-wide hover:shadow-xl rounded-md outline-none ">Subscribe</button>
                 </div>
               </form>
             </div>
